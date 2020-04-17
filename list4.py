7756256515ef7e235797682139026ab24354a845
kubex909 <daniel_r1@wp.pl>
2020-04-17 15:27:28 +0200

    add wallet

--- list4.py
+++ list4.py
@@ -24,7 +24,7 @@ def get_price():
     blockchain = requests.get("https://blockchain.info/ticker")
     return bitbay.json(),cex.json(),bitstamp.json(),blockchain.json()
 
-def buy_sell():
+def buy_sell(amount):
     bitbay,cex,bitstamp,blockchain=get_price()
 
     
@@ -42,29 +42,49 @@ def buy_sell():
     blockchain_sell = blockchain["USD"]["sell"]
 
     name=['bitbay','cex','bitstamp','blockchain']
+    name2=['bitbay','cex','bitstamp','blockchain']
     taker=[1.03,1.03,1.025,1.024]
+    indeks=[4,9,14,19]
+
     buy=[bitbay_buy,cex_buy,bitstamp_buy,blockchain_buy]
     sell=[bitbay_sell,cex_sell,bitstamp_sell,blockchain_sell]
+    print(buy)
+    print (sell)
     for i in range(len(taker)):
-        buy[i]=(taker[i]*buy[i]*0.2)
-        sell[i]=(taker[i]*sell[i]*0.2)
-        
+        buy[i]=((1-taker[i]+1)*buy[i]*amount)
+        sell[i]=(taker[i]*sell[i]*amount)
+
+    print(buy)
+    print (sell)
+
     buy=max(zip(buy,name))
     same_index=name.index(buy[1])
-
+    
     sell.pop(same_index)
     name.pop(same_index)
     sell=min(zip(sell,name))
 
+    indeks_add=indeks[same_index]
+    same_index=name2.index(sell[1])
+    indeks_minus=indeks[same_index]
+
     if buy[0]>sell[0]:
         print("sprzedaj 0,2 BTC w", buy[1],"za",buy[0], "kup w" ,sell[1],"za", sell[0],"zyskując", buy[0]-sell[0], "USD")
         wallet_file = open('C:/git/Python-course-SI/wallet.txt','r+')
-        print (wallet_file.read())
         wallet = wallet_file.readlines()
+        if  float(wallet[indeks_add+2])>amount and float(wallet[indeks_minus])>sell[0]:
+            
+            wallet[1]=str(float(wallet[1])+ buy[0]-sell[0])+"\n"
+            wallet[indeks_add+2]=str(float(wallet[indeks_add+2])-amount)+"\n"
+            wallet[indeks_add]=str(float(wallet[indeks_add])+buy[0])+"\n"
+            wallet[indeks_minus+2]=str(float(wallet[indeks_minus+2])+amount)
+            wallet[indeks_minus]=str(float(wallet[indeks_minus])-sell[0])+"\n"
 
-    
-
+            wallet_file.seek(0)
+            wallet_file.writelines(wallet)
+        else:
+            print("brak srodkow na koncie")
         wallet_file.close()
     else:
         print("nie ma możliwości arbitrażu")
-buy_sell()
+buy_sell(0.2)
